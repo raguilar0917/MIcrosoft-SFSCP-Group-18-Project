47950bbce7fef9f6622a99f8a88b442735bd660e
Richard <codyrwa2011@me.com>
2021-07-27 22:09:51 -0700

    deployment

---
+++ build/precache-manifest.890e292ba82d1b60798e13570a3e3926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb658084dc654e67677d0f2036590ed8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "17f0fce5dbf5179e2581",
+    "url": "/react-gh-pages/static/css/main.cca13aff.chunk.css"
+  },
+  {
+    "revision": "deae42d8de7a2e23c23e",
+    "url": "/react-gh-pages/static/js/2.0570ad0a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-gh-pages/static/js/2.0570ad0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f0fce5dbf5179e2581",
+    "url": "/react-gh-pages/static/js/main.6b43164c.chunk.js"
+  },
+  {
+    "revision": "cff4966449de142af2be",
+    "url": "/react-gh-pages/static/js/runtime-main.ce25c441.js"
+  }
+]);
