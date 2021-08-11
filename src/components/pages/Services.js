981678059ee6552b92c053c38d04635aa1a23c20
Richard Aguilar <codyrwa2011@me.com>
2021-08-10 23:41:08 -0700

    Alert Update
    Removed the alert.js and added the google api

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import '../../App.css';
-import AlertApp from '../AlertApp';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-function Services() {
-  return (
-    <>
-      <AlertApp />
-    </>
-  );
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+
+var sfLat = 37.7241, sfLong = -122.4799;
+
+class SimpleMap extends Component {
+ 
+
+ componentDidMount() {
+  }
+
+ static defaultProps = {
+    center: {
+      lat: sfLat,
+      lng: sfLong
+    },
+    zoom: 11,
+    mapId: '983106035cd0f095'
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDPLGQ2-8F_lzs13ZgYbKvLosRafqE5tJs" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={sfLat}
+            lng={sfLong}
+            text="Your Location: San Francisco State University"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
-export default Services;
+export default SimpleMap;
